da53a09d1541b72a8d8cfc0c099f656986bdd051
Paul Allcca Flores <paul.flores@upb.edu.co>
2021-08-29 20:50:45 +0000

    add Main fileIn and fileOut

--- main.c
+++ main.c
@@ -2,7 +2,36 @@
 #include <stdlib.h>
 #include "student.h"
 
+
+/* main es el nombre del archivo ejecutable.
+|  pathInFileName y pathOutFileName son dos cadenas con el path absoluto o 
+   relativo a los archivos de entrada y de salida respectivamente.
+*/
+
 int main(int argc, char *argv[]){
+
+    FILE *fileIn = NULL;
+    FILE *fileOut = NULL;
+
+    for(u_int8_t i = 0; i < argc; i++){
+        if (i == 1){
+            fileIn = fopen(argv[1],"r");
+            if (fileIn == NULL){
+                perror("Open fileIn file fails: ");
+            return(EXIT_FAILURE);
+            }
+        }
+    
+        if(i == 2){
+            fileOut = fopen(argv[i], "r+");
+            if (fileOut == NULL){
+                perror("Out fileOut file fails: ");
+                return(EXIT_FAILURE);
+            }
+        }
+    }
+    feature1(fileIn,fileOut);
+    feature2(fileIn,fileOut);
 
     return EXIT_SUCCESS;
 }
